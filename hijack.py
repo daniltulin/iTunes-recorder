2d7f45ed87c69a51c1d59f62b0682956e165bacd
tulindanil <tulindanil@gmail.com>
2016-09-28 13:51:35 +0300

    Import libraries add LaunchException and Event classes

--- hijack.py
+++ hijack.py
@@ -1,12 +1,28 @@
-
 from os import system, path
 from subprocess import Popen, PIPE
+from os.path import join
+from datetime import datetime
+from plistlib import dump, load
 
+class LaunchException(Exception): pass
 
-APP_PATH = '/Applications/Audio\ Hijack.app/'
-META_PATH = path.expanduser('~/Library/Application\ Support/Audio\ Hijack/')
+APP_PATH = '/Applications/Audio\ Hijack.app'
+META_PATH = path.expanduser('~/Library/Application Support/Audio Hijack')
+SCHEDULE_PATH = join(META_PATH, 'Schedule.plist')
 
-class LaunchError(Exception): pass
+class Event(dict):
+    def __init__(self, track):
+        now = datetime.now()
+        single_date = datetime(now.year, now.month, now.day, 0, 0)
+        start_time = now - single_date
+        super(Event, self).__init__(scheduleRepeatDays=0,
+                                    scheduleMode=0,
+                                    scheduleEnabled=True,
+                                    scheduleSingleDate=single_date,
+                                    scheduleDuration=track.duration() + 1,
+                                    scheduleQuitSources=False)
+        self['sessionUUID'] = '0B2BCFF7-2D43-4F74-A393-FDAADCA633B8'
+        self['scheduleStartTime'] = start_time.seconds
 
 class Jack:
     def start_recording(self, track):
